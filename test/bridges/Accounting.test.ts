54c26a40a9cc7d1f6484eb0ac672a2b359487c6f
Shane Fontaine <shane6fontaine@gmail.com>
2021-02-02 20:29:47 -0800

    Add non-happy path and edg-ecase tests

--- test/bridges/Accounting.test.ts
+++ test/bridges/Accounting.test.ts
@@ -12,6 +12,8 @@ describe("Accounting", () => {
   let _fixture: IFixture
 
   let bonder: Signer
+  let user: Signer
+  let otherAccount: Signer
 
   let mockAccounting: Contract
 
@@ -22,6 +24,8 @@ describe("Accounting", () => {
 
     ;({ 
       bonder,
+      user,
+      otherAccount,
       mockAccounting
     } = _fixture);
   })
@@ -37,19 +41,19 @@ describe("Accounting", () => {
   })
 
   it('Should get the correct credit', async () => {
-    const expectedCredit = BigNumber.from(0)
+    const expectedCredit: BigNumber = BigNumber.from(0)
     const credit = await mockAccounting.getCredit()
     expect(credit).to.eq(expectedCredit)
   })
 
   it('Should get the correct debit', async () => {
-    const expectedDebit = BigNumber.from(0)
+    const expectedDebit: BigNumber = BigNumber.from(0)
     const debit = await mockAccounting.getDebit()
     expect(debit).to.eq(expectedDebit)
   })
 
   it('Should stake and increase the credit', async () => {
-    const stakeAmount = BigNumber.from(10)
+    const stakeAmount: BigNumber = BigNumber.from(10)
 
     await mockAccounting.stake(stakeAmount)
     let credit = await mockAccounting.getCredit()
@@ -65,7 +69,7 @@ describe("Accounting", () => {
   })
 
   it('Should stake to increase the credit and subsequently unstake to increase the debit', async () => {
-    const stakeAmount = BigNumber.from(10)
+    const stakeAmount: BigNumber = BigNumber.from(10)
 
     await mockAccounting.stake(stakeAmount)
     let credit = await mockAccounting.getCredit()
@@ -92,26 +96,45 @@ describe("Accounting", () => {
     expect(debit).to.eq(stakeAmount.mul(2))
   })
 
+  it('Should stake many times with different users and then unstake', async () => {
+    const stakeAmount: BigNumber = BigNumber.from(10)
+
+    await mockAccounting.connect(bonder).stake(stakeAmount)
+    await mockAccounting.connect(user).stake(stakeAmount)
+    await mockAccounting.connect(otherAccount).stake(stakeAmount)
+
+    let credit = await mockAccounting.getCredit()
+    let debit = await mockAccounting.getDebit()
+    expect(credit).to.eq(stakeAmount.mul(3))
+    expect(debit).to.eq(0)
+
+    await mockAccounting.connect(bonder).unstake(stakeAmount.mul(3))
+    credit = await mockAccounting.getCredit()
+    debit = await mockAccounting.getDebit()
+    expect(credit).to.eq(stakeAmount.mul(3))
+    expect(debit).to.eq(stakeAmount.mul(3))
+  })
+
   /**
    * Non-Happy Path
    */
 
   it('Should not allow a higher debit than credit', async () => {
-    const expectedError = 'ACT: Not enough available credit'
-    const stakeAmount = BigNumber.from(10)
+    const expectedErrorMsg: string = 'ACT: Not enough available credit'
+    const stakeAmount: BigNumber = BigNumber.from(10)
 
     await expect(
       mockAccounting.connect(bonder).unstake(stakeAmount)
-    ).to.be.revertedWith(expectedError)
+    ).to.be.revertedWith(expectedErrorMsg)
   })
 
   it('Should not allow someone outside of the bonder to unstake', async () => {
-    const expectedError = 'ACT: Caller is not bonder'
-    const stakeAmount = BigNumber.from(10)
+    const expectedErrorMsg: string = 'ACT: Caller is not bonder'
+    const stakeAmount: BigNumber = BigNumber.from(10)
 
     await mockAccounting.stake(stakeAmount)
     await expect(
       mockAccounting.unstake(stakeAmount)
-    ).to.be.revertedWith(expectedError)
+    ).to.be.revertedWith(expectedErrorMsg)
   })
 })
--- test/bridges/Bridge.test.ts
+++ test/bridges/Bridge.test.ts
@@ -1,6 +1,7 @@
 import '@nomiclabs/hardhat-waffle'
 import { expect } from 'chai'
-import { Contract, BigNumber } from 'ethers'
+import { Contract, BigNumber, Signer } from 'ethers'
+import MerkleTree from '../../lib/MerkleTree'
 import Transfer from '../../lib/Transfer'
 
 import { fixture } from '../shared/fixtures'
@@ -15,8 +16,10 @@ describe("Bridge", () => {
   let mockBridge: Contract
   let transfers: Transfer[]
 
+  let l2ChainId: BigNumber
+
   beforeEach(async () => {
-    const l2ChainId: BigNumber = CHAIN_IDS.OPTIMISM.TESTNET_1
+    l2ChainId = CHAIN_IDS.OPTIMISM.TESTNET_1
     _fixture = await fixture(l2ChainId)
     await setUpDefaults(_fixture, l2ChainId)
 
@@ -57,6 +60,15 @@ describe("Bridge", () => {
     expect(amountHash).to.eq('0x' + expectedAmountHash.toString('hex'))
   })
 
+  it('Should get the correct amount hash with arbitrary values and array lengths', async () => {
+    const chainIds: Number[] = [10, 79377087078960]
+    const amounts: Number[] = [123, 999, 1, 2, 3, 4, 5, 6]
+
+    const expectedAmountHash: Buffer = generateAmountHash(chainIds, amounts)
+    const amountHash = await mockBridge.getAmountHash(chainIds, amounts)
+    expect(amountHash).to.eq('0x' + expectedAmountHash.toString('hex'))
+  })
+
   it('Should get the correct chainId', async () => {
     const expectedChainId = 1
     const chainId = await mockBridge.getChainId()
@@ -67,6 +79,52 @@ describe("Bridge", () => {
    * Non-Happy Path
    */
 
-   // TODO
+  it('Should not allow a withdrawal because of an invalid proof', async () => {
+    const transfer: Transfer = transfers[0]
+    const arbitraryRootHash: string = '0x7465737400000000000000000000000000000000000000000000000000000000'
+    const arbitraryProof: string[] = [arbitraryRootHash, arbitraryRootHash]
+
+    const expectedErrorMsg: string = 'BRG: Invalid transfer proof'
+
+    await expect(
+      mockBridge.withdraw(
+        transfer.sender,
+        transfer.recipient,
+        transfer.amount,
+        transfer.transferNonce,
+        transfer.relayerFee,
+        arbitraryRootHash,
+        arbitraryProof
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
+  })
+
+  it.only('Should not allow a withdrawal because the transfer root is not found', async () => {
+    const transfer: Transfer = transfers[0]
+
+    // Set up transfer 
+    transfer.chainId = await mockBridge.getChainId()
+    transfer.amountOutMin = BigNumber.from(0)
+    transfer.deadline = BigNumber.from(0)
+
+    const transferHash: Buffer = transfer.getTransferHash()
+    const tree: MerkleTree = new MerkleTree([ transferHash ])
+    const transferRootHash: Buffer = tree.getRoot()
+    const proof: Buffer[] = tree.getProof(transferHash)
+
+    const expectedErrorMsg: string = 'BRG: Transfer root not found'
+
+    await expect(
+      mockBridge.withdraw(
+        transfer.sender,
+        transfer.recipient,
+        transfer.amount,
+        transfer.transferNonce,
+        transfer.relayerFee,
+        transferRootHash,
+        proof
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
+  })
 
 })
