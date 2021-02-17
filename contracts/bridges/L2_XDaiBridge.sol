deac914b16f8df98f77cf31cebe751add366d11f
cwhinfrey <cwhinfrey@gmail.com>
2021-02-16 16:36:07 -0800

    Add xDAI L2 Bridge and messenger wrapper

---
+++ contracts/bridges/L2_XDaiBridge.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+
+import "../interfaces/xDai/messengers/iArbitraryMessageBridge.sol";
+import "./L2_Bridge.sol";
+
+contract L2_XDaiBridge is L2_Bridge {
+    iArbitraryMessageBridge public messenger;
+    /// @notice The xDai AMB uses bytes32 for chainId instead of uint256
+    bytes32 public l1ChainId;
+
+    /// @notice Always the L1 chain id first in supportedChainIds
+    constructor (
+        iArbitraryMessageBridge _messenger,
+        address l1Governance,
+        IERC20 canonicalToken,
+        address l1BridgeAddress,
+        uint256[] memory supportedChainIds,
+        address[] memory bonders,
+        address exchangeAddress,
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    )
+        public
+        L2_Bridge(
+            l1Governance,
+            canonicalToken,
+            l1BridgeAddress,
+            supportedChainIds,
+            bonders,
+            exchangeAddress,
+            name,
+            symbol,
+            decimals
+        )
+    {
+        messenger = _messenger;
+        l1ChainId = bytes32(supportedChainIds[0]);
+    }
+
+    // TODO: Use a valid gasLimit
+    function _sendCrossDomainMessage(bytes memory message) internal override {
+        messenger.requireToPassMessage(
+            l1BridgeAddress,
+            message,
+            0
+        );
+    }
+
+    function _verifySender(address expectedSender) internal override {
+        require(messenger.messageSender() == expectedSender);
+
+        // With the xDai AMB, it is best practice to also check the source chainId
+        // https://docs.tokenbridge.net/amb-bridge/how-to-develop-xchain-apps-by-amb#receive-a-method-call-from-the-amb-bridge
+        require(messenger.messageSourceChainId() == l1ChainId);
+    }
+}
