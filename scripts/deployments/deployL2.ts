e892c990bf38efd3b6e584a6077d1dd1cfb39937
cwhinfrey <cwhinfrey@gmail.com>
2021-03-25 15:04:50 -0700

    contracts: Update deploy scripts for amm changes

--- scripts/deployments/deployL2.ts
+++ scripts/deployments/deployL2.ts
@@ -73,20 +73,16 @@ export async function deployL2 (config: Config) {
   let L2_MockERC20: ContractFactory
   let L2_HopBridgeToken: ContractFactory
   let L2_Bridge: ContractFactory
-  let L2_UniswapFactory: ContractFactory
-  let L2_UniswapRouter: ContractFactory
-  let L2_UniswapPair: ContractFactory
-  let L2_UniswapWrapper: ContractFactory
+  let L2_Swap: ContractFactory
+  let L2_AmmWrapper: ContractFactory
 
   // Contracts
   let l1_bridge: Contract
   let l2_bridge: Contract
   let l2_canonicalToken: Contract
   let l2_hopBridgeToken: Contract
-  let l2_uniswapFactory: Contract
-  let l2_uniswapRouter: Contract
-  let l2_uniswapPair: Contract
-  let l2_uniswapWrapper: Contract
+  let l2_swap: Contract
+  let l2_ammWrapper: Contract
 
   // Instantiate the wallets
   accounts = await ethers.getSigners()
@@ -106,10 +102,8 @@ export async function deployL2 (config: Config) {
     L2_MockERC20,
     L2_HopBridgeToken,
     L2_Bridge,
-    L2_UniswapFactory,
-    L2_UniswapRouter,
-    L2_UniswapPair,
-    L2_UniswapWrapper
+    L2_Swap,
+    L2_AmmWrapper
   } = await getContractFactories(l2_chainId, owner, ethers, ovmEthers))
 
   logger.log('attaching deployed contracts')
@@ -129,43 +123,34 @@ export async function deployL2 (config: Config) {
   )
   await waitAfterTransaction(l2_hopBridgeToken, ethers)
 
-  logger.log('deploying L2 uniswap factory and L2 uniswap router')
-  ;({ l2_uniswapFactory, l2_uniswapRouter } = await deployUniswap(
+  logger.log('deploying L2 swap contract')
+  ;({ l2_swap } = await deployAmm(
     ethers,
-    owner,
-    L2_UniswapFactory,
-    L2_UniswapRouter,
-    l2_uniswapFactory,
-    l2_uniswapRouter
+    l2_chainId,
+    l2_canonicalToken,
+    l2_hopBridgeToken,
+    L2_Swap
   ))
 
-  logger.log('deploying L2 bridge and L2 uniswap wrapper')
-  ;({ l2_bridge, l2_uniswapWrapper } = await deployBridge(
+  logger.log('deploying L2 bridge and L2 amm wrapper')
+  ;({ l2_bridge, l2_ammWrapper } = await deployBridge(
     l2_chainId,
     l1_chainId,
     ethers,
     owner,
     bonder,
     L2_Bridge,
-    L2_UniswapWrapper,
+    L2_AmmWrapper,
     l1_bridge,
     l2_bridge,
     l2_hopBridgeToken,
     l2_canonicalToken,
-    l2_uniswapRouter,
-    l2_uniswapWrapper,
+    l2_swap,
+    l2_ammWrapper,
     l2_messengerAddress
   ))
 
   logger.log('deploying network specific contracts')
-  await deployNetworkSpecificContracts(
-    l2_chainId,
-    owner,
-    ethers,
-    L2_UniswapPair,
-    l2_uniswapFactory,
-    l2_uniswapPair
-  )
 
   // Transfer ownership of the Hop Bridge Token to the L2 Bridge
   let transferOwnershipParams: any[] = [l2_bridge.address]
@@ -180,55 +165,77 @@ export async function deployL2 (config: Config) {
 
   const l2_hopBridgeTokenAddress: string = l2_hopBridgeToken.address
   const l2_bridgeAddress: string = l2_bridge.address
-  const l2_uniswapFactoryAddress: string = l2_uniswapFactory.address
-  const l2_uniswapRouterAddress: string = l2_uniswapRouter.address
-  const l2_uniswapWrapperAddress: string = l2_uniswapWrapper.address
+  const l2_swapAddress: string = l2_swap.address
+  const l2_ammWrapperAddress: string = l2_ammWrapper.address
 
   logger.log('L2 Deployments Complete')
   logger.log('L2 Hop Bridge Token :', l2_hopBridgeTokenAddress)
   logger.log('L2 Bridge           :', l2_bridgeAddress)
-  logger.log('L2 Uniswap Factory  :', l2_uniswapFactoryAddress)
-  logger.log('L2 Uniswap Router   :', l2_uniswapRouterAddress)
-  logger.log('L2 Uniswap Wrapper  :', l2_uniswapWrapperAddress)
+  logger.log('L2 Swap  :', l2_swapAddress)
+  logger.log('L2 Amm Wrapper  :', l2_ammWrapperAddress)
 
   updateConfigFile({
     l2_hopBridgeTokenAddress,
     l2_bridgeAddress,
-    l2_uniswapFactoryAddress,
-    l2_uniswapRouterAddress,
-    l2_uniswapWrapperAddress
+    l2_swapAddress,
+    l2_ammWrapperAddress
   })
 
   return {
     l2_hopBridgeToken,
     l2_bridgeAddress,
-    l2_uniswapFactoryAddress,
-    l2_uniswapRouterAddress
+    l2_swapAddress
   }
 }
 
-const deployUniswap = async (
+const deployAmm = async (
   ethers: any,
-  owner: Signer,
-  L2_UniswapFactory: ContractFactory,
-  L2_UniswapRouter: ContractFactory,
-  l2_uniswapFactory: Contract,
-  l2_uniswapRouter: Contract
+  l2_chainId: BigNumber,
+  l2_canonicalToken: Contract,
+  l2_hopBridgeToken: Contract,
+  L2_Swap: ContractFactory,
 ) => {
-  l2_uniswapFactory = await L2_UniswapFactory.connect(owner).deploy(
-    await owner.getAddress()
-  )
-  await waitAfterTransaction(l2_uniswapFactory, ethers)
 
-  l2_uniswapRouter = await L2_UniswapRouter.connect(owner).deploy(
-    l2_uniswapFactory.address,
-    ZERO_ADDRESS
-  )
-  await waitAfterTransaction(l2_uniswapRouter, ethers)
+  let decimalParams: any[] = []
+
+  if (isChainIdXDai(l2_chainId)) {
+    decimalParams.push(overrides)
+  }
+
+  const l2_canonicalTokenDecimals = await l2_canonicalToken.decimals(...decimalParams)
+  const l2_hopBridgeTokenDecimals = await l2_hopBridgeToken.decimals(...decimalParams)
+
+  // Deploy AMM contracts
+
+  const l2_swap = await L2_Swap.deploy()
+  await waitAfterTransaction(l2_swap, ethers)
+
+  console.log('### deployed')
+
+  let initializeParams: any[] = [
+    [l2_canonicalToken.address, l2_hopBridgeToken.address],
+    [l2_canonicalTokenDecimals, l2_hopBridgeTokenDecimals],
+    'Hop DAI LP Token',
+    'HOP-LP-DAI',
+    '200',
+    '4000000',
+    '0',
+    '0'
+  ]
+
+  if (isChainIdXDai(l2_chainId)) {
+    initializeParams.push(overrides)
+  }
+
+  // ToDo: Pass in true token name and symbol
+  const tx = await l2_swap.initialize(...initializeParams)
+  await tx.wait()
+  await waitAfterTransaction()
+
+  console.log('### initialized')
 
   return {
-    l2_uniswapFactory,
-    l2_uniswapRouter
+    l2_swap
   }
 }
 
@@ -239,13 +246,13 @@ const deployBridge = async (
   owner: Signer,
   bonder: Signer,
   L2_Bridge: ContractFactory,
-  L2_UniswapWrapper: ContractFactory,
+  L2_AmmWrapper: ContractFactory,
   l1_bridge: Contract,
   l2_bridge: Contract,
   l2_hopBridgeToken: Contract,
   l2_canonicalToken: Contract,
-  l2_uniswapRouter: Contract,
-  l2_uniswapWrapper: Contract,
+  l2_swap: Contract,
+  l2_ammWrapper: Contract,
   l2_messengerAddress: string
 ) => {
   // NOTE: Adding more CHAIN_IDs here will push the OVM deployment over the contract size limit
@@ -267,56 +274,18 @@ const deployBridge = async (
 
   const l2CanonicalTokenName = await l2_canonicalToken.symbol(overrides)
   const l2CanonicalTokenIsEth: boolean = l2CanonicalTokenName === 'WETH'
-  l2_uniswapWrapper = await L2_UniswapWrapper.connect(owner).deploy(
+  l2_ammWrapper = await L2_AmmWrapper.connect(owner).deploy(
     l2_bridge.address,
     l2_canonicalToken.address,
     l2CanonicalTokenIsEth,
     l2_hopBridgeToken.address,
-    l2_uniswapRouter.address
+    l2_swap.address
   )
-  await waitAfterTransaction(l2_uniswapWrapper, ethers)
+  await waitAfterTransaction(l2_ammWrapper, ethers)
 
   return {
     l2_bridge,
-    l2_uniswapWrapper
-  }
-}
-
-const deployNetworkSpecificContracts = async (
-  l2ChainId: BigNumber,
-  owner: Signer,
-  ethers: any,
-  L2_UniswapPair: ContractFactory,
-  l2_uniswapFactory: Contract,
-  l2_uniswapPair: Contract
-) => {
-  if (isChainIdXDai(l2ChainId)) {
-    // No network specific deployments
-  }
-
-  if (isChainIdArbitrum(l2ChainId)) {
-    // No network specific deployments
-  }
-
-  if (isChainIdOptimism(l2ChainId)) {
-    l2_uniswapPair = await L2_UniswapPair.connect(owner).deploy(
-      l2_uniswapFactory.address
-    )
-    await waitAfterTransaction(l2_uniswapPair, ethers)
-
-    let setPairParams: any[] = [l2_uniswapPair]
-    if (isChainIdXDai(l2ChainId)) {
-      setPairParams.push(overrides)
-    }
-    const tx = await l2_uniswapFactory
-      .connect(owner)
-      .setPair(l2_uniswapPair.address)
-    await tx.wait()
-    await waitAfterTransaction()
-    const realPair = await l2_uniswapFactory.realPair()
-    if (l2_uniswapPair.address !== realPair) {
-      throw new Error('Pair did not get set on the factory.')
-    }
+    l2_ammWrapper
   }
 }
 
