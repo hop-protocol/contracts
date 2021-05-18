d47c0eba41142e81f2832a055f8d726a2d427bb2
Shane Fontaine <shane6fontaine@gmail.com>
2021-05-17 18:42:04 -0700

    Add SPDX licenses

--- contracts/test/arbitrum/MockEthERC20Bridge.sol
+++ contracts/test/arbitrum/MockEthERC20Bridge.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.6.12;
 
 contract MockEthERC20Bridge {
--- contracts/test/xDai/Mock_L1_xDaiMessenger.sol
+++ contracts/test/xDai/Mock_L1_xDaiMessenger.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity 0.6.12;
 
 contract Mock_L1_xDaiMessenger {
