24a958ea8d7b16f773eb94877b8dd950282e116e
Shane Fontaine <shane6fontaine@gmail.com>
2021-05-17 20:39:01 -0700

    Add Polygon and xDai messenger wrapper tests

--- test/wrappers/PolygonWrapper.test.ts
+++ test/wrappers/PolygonWrapper.test.ts
@@ -1,6 +1,6 @@
 import '@nomiclabs/hardhat-waffle'
 import { expect } from 'chai'
-import { Contract, BigNumber } from 'ethers'
+import { Contract, BigNumber, Signer, utils as ethersUtils } from 'ethers'
 
 import { fixture } from '../shared/fixtures'
 import { setUpDefaults } from '../shared/utils'
@@ -8,24 +8,33 @@ import { IFixture } from '../shared/interfaces'
 
 import {
   CHAIN_IDS,
-  DEFAULT_MESSENGER_WRAPPER_GAS_LIMIT,
-  DEFAULT_MESSENGER_WRAPPER_GAS_PRICE,
-  DEFAULT_MESSENGER_WRAPPER_CALL_VALUE,
-  ZERO_ADDRESS
+  ONE_ADDRESS
 } from '../../config/constants'
+
+import {
+  getPolygonCheckpointManagerAddress
+} from '../../config/utils'
+
+import {
+  getSetL1BridgeAddressMessage
+} from '../shared/contractFunctionWrappers'
 
 export const MAX_NUM_SENDS_BEFORE_COMMIT = 10
 
-describe.skip('Polygon Wrapper', () => {
+describe('Polygon Wrapper', () => {
   let _fixture: IFixture
+
+  let user: Signer
 
   let l1ChainId: BigNumber
   let l2ChainId: BigNumber
 
-  let l1_messenger: Contract
   let l1_bridge: Contract
   let l1_messengerWrapper: Contract
-  let l2_bridge: Contract
+  let l2_messenger: Contract
+  let l2_messengerProxy: Contract
+
+  let fxRoot: Contract
 
   beforeEach(async () => {
     l1ChainId = CHAIN_IDS.ETHEREUM.GOERLI
@@ -33,7 +42,14 @@ describe.skip('Polygon Wrapper', () => {
 
     _fixture = await fixture(l1ChainId, l2ChainId)
     await setUpDefaults(_fixture)
-    ;({ l1_messenger, l1_bridge, l1_messengerWrapper, l2_bridge } = _fixture)
+    ;({
+      user,
+      l1_bridge,
+      l1_messengerWrapper,
+      l2_messenger,
+      l2_messengerProxy,
+      fxRoot
+    } = _fixture)
   })
 
   /**
@@ -41,29 +57,74 @@ describe.skip('Polygon Wrapper', () => {
    */
 
   it('Should set the correct values in the constructor', async () => {
-    // TODO
     const expectedL1BridgeAddress: string = l1_bridge.address
-    const expectedL2BridgeAddress: string = l2_bridge.address
-    const expectedDefaultGasLimit: number = DEFAULT_MESSENGER_WRAPPER_GAS_LIMIT
-    const expectedArbInbox: string = l1_messenger.address
-    const expectedArbBridge: string = ZERO_ADDRESS
-    const expectedDefaultGasPrice: number = DEFAULT_MESSENGER_WRAPPER_GAS_PRICE
-    const expectedDefaultCallValue: number = DEFAULT_MESSENGER_WRAPPER_CALL_VALUE
+    const expectedCheckpointManager: string = getPolygonCheckpointManagerAddress(l1ChainId)
+    const expectedFxRoot: string = fxRoot.address
+    const expectedFxChildTunnel: string = l2_messengerProxy.address
 
     const l1BridgeAddress: string = await l1_messengerWrapper.l1BridgeAddress()
-    const l2BridgeAddress: string = await l1_messengerWrapper.l2BridgeAddress()
-    const defaultGasLimit: number = await l1_messengerWrapper.defaultGasLimit()
-    const arbInbox: string = await l1_messengerWrapper.arbInbox()
-    const arbBridge: string = await l1_messengerWrapper.arbBridge()
-    const defaultGasPrice: string = await l1_messengerWrapper.defaultGasPrice()
-    const defaultCallValue: string = await l1_messengerWrapper.defaultCallValue()
+    const checkpointManager: string = await l1_messengerWrapper.checkpointManager()
+    const fxRootAddress: string = await l1_messengerWrapper.fxRoot()
+    const fxChildTunnel: string = await l1_messengerWrapper.fxChildTunnel()
 
     expect(expectedL1BridgeAddress).to.eq(l1BridgeAddress)
-    expect(expectedL2BridgeAddress).to.eq(l2BridgeAddress)
-    expect(expectedDefaultGasLimit).to.eq(defaultGasLimit)
-    expect(expectedArbInbox).to.eq(arbInbox)
-    expect(expectedArbBridge).to.eq(arbBridge)
-    expect(expectedDefaultGasPrice).to.eq(defaultGasPrice)
-    expect(expectedDefaultCallValue).to.eq(defaultCallValue)
+    expect(expectedCheckpointManager).to.eq(checkpointManager)
+    expect(expectedFxRoot).to.eq(fxRootAddress)
+    expect(expectedFxChildTunnel).to.eq(fxChildTunnel)
   })
+
+  it('Should allow anyone to send a cross domain message', async () => {
+    const message: string = getSetL1BridgeAddressMessage(ONE_ADDRESS)
+    await l1_messengerWrapper.connect(user).sendCrossDomainMessage(message)
+
+    const messengerWrapperMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address', 'bytes'],
+      [await user.getAddress(), message]
+    )
+
+    const fxRootMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address', 'address', 'bytes'],
+      [l1_messengerWrapper.address, l2_messengerProxy.address, messengerWrapperMessage]
+    )
+
+    const actualNextMessage: string = (await l2_messenger.nextMessage()).message
+    expect(fxRootMessage).to.eq(actualNextMessage)
+  })
+
+  it('Should verify the sender', async () => {
+    // If this function succeeds then the test is a success
+    const arbitraryMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address'],
+      [ONE_ADDRESS]
+    )
+    await l1_messengerWrapper.verifySender(l1_messengerWrapper.address, arbitraryMessage)
+  })
+
+  /**
+   * Happy Path
+   */
+
+  it('Should throw because the sender is invalid', async () => {
+    const expectedErrorMsg: string = 'L1_PLGN_WPR: Caller must be this contract'
+    const arbitraryMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address'],
+      [ONE_ADDRESS]
+    )
+    await expect(
+      l1_messengerWrapper.verifySender(
+        ONE_ADDRESS,
+        arbitraryMessage
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
+  })
+
+  it('Should throw because the fxChildTunnel can only be set once', async () => {
+    const expectedErrorMsg: string = 'FxBaseRootTunnel: CHILD_TUNNEL_ALREADY_SET'
+    await expect(
+      l1_messengerWrapper.setFxChildTunnel(
+        ONE_ADDRESS
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
+  })
+
 })
--- test/wrappers/XDaiMessengerWrapper.test.ts
+++ test/wrappers/XDaiMessengerWrapper.test.ts
@@ -1,22 +1,24 @@
 import '@nomiclabs/hardhat-waffle'
 import { expect } from 'chai'
-import { Contract, BigNumber } from 'ethers'
+import { Contract, BigNumber, utils as ethersUtils } from 'ethers'
 
 import { fixture } from '../shared/fixtures'
 import { setUpDefaults } from '../shared/utils'
 import { IFixture } from '../shared/interfaces'
+import { getXDaiAmbAddresses } from '../../config/utils'
 
 import {
   CHAIN_IDS,
   DEFAULT_MESSENGER_WRAPPER_GAS_LIMIT,
   DEFAULT_MESSENGER_WRAPPER_GAS_PRICE,
   DEFAULT_MESSENGER_WRAPPER_CALL_VALUE,
-  ZERO_ADDRESS
+  ZERO_ADDRESS,
+  ONE_ADDRESS
 } from '../../config/constants'
 
 export const MAX_NUM_SENDS_BEFORE_COMMIT = 10
 
-describe.skip('XDai Messenger Wrapper', () => {
+describe('XDai Messenger Wrapper', () => {
   let _fixture: IFixture
 
   let l1ChainId: BigNumber
@@ -41,29 +43,56 @@ describe.skip('XDai Messenger Wrapper', () => {
    */
 
   it('Should set the correct values in the constructor', async () => {
-    // TODO
     const expectedL1BridgeAddress: string = l1_bridge.address
     const expectedL2BridgeAddress: string = l2_bridge.address
-    const expectedDefaultGasLimit: number = DEFAULT_MESSENGER_WRAPPER_GAS_LIMIT
-    const expectedArbInbox: string = l1_messenger.address
-    const expectedArbBridge: string = ZERO_ADDRESS
-    const expectedDefaultGasPrice: number = DEFAULT_MESSENGER_WRAPPER_GAS_PRICE
-    const expectedDefaultCallValue: number = DEFAULT_MESSENGER_WRAPPER_CALL_VALUE
+    const expectedL1MessengerAddress: string = l1_messenger.address
+    const expectedDefaultGasLimit: number = 1000000
+    const expectedL2ChainId: string = ethersUtils.hexZeroPad(l2ChainId.toHexString(), 32)
+    const expectedAmbBridge: string = getXDaiAmbAddresses(true)
 
     const l1BridgeAddress: string = await l1_messengerWrapper.l1BridgeAddress()
     const l2BridgeAddress: string = await l1_messengerWrapper.l2BridgeAddress()
+    const l1MessengerAddress: string = await l1_messengerWrapper.l1MessengerAddress()
     const defaultGasLimit: number = await l1_messengerWrapper.defaultGasLimit()
-    const arbInbox: string = await l1_messengerWrapper.arbInbox()
-    const arbBridge: string = await l1_messengerWrapper.arbBridge()
-    const defaultGasPrice: string = await l1_messengerWrapper.defaultGasPrice()
-    const defaultCallValue: string = await l1_messengerWrapper.defaultCallValue()
+    const actualL2ChainId: string = await l1_messengerWrapper.l2ChainId()
+    const ambBridge: string = await l1_messengerWrapper.ambBridge()
 
     expect(expectedL1BridgeAddress).to.eq(l1BridgeAddress)
     expect(expectedL2BridgeAddress).to.eq(l2BridgeAddress)
+    expect(expectedL1MessengerAddress).to.eq(l1MessengerAddress)
     expect(expectedDefaultGasLimit).to.eq(defaultGasLimit)
-    expect(expectedArbInbox).to.eq(arbInbox)
-    expect(expectedArbBridge).to.eq(arbBridge)
-    expect(expectedDefaultGasPrice).to.eq(defaultGasPrice)
-    expect(expectedDefaultCallValue).to.eq(defaultCallValue)
+    expect(expectedL2ChainId).to.eq(actualL2ChainId)
+    expect(expectedAmbBridge).to.eq(ambBridge)
+  })
+
+  /**
+   * Non-happy Path
+   */
+
+  it('Should throw because the sender is invalid', async () => {
+    const expectedErrorMsg: string = 'MW: Sender must be the L1 Bridge'
+    const arbitraryMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address'],
+      [ONE_ADDRESS]
+    )
+    await expect(
+      l1_messengerWrapper.sendCrossDomainMessage(
+        arbitraryMessage
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
+  })
+
+  it('Should throw because messageSender() is invalid', async () => {
+    const expectedErrorMsg: string = 'L2_XDAI_BRG: Invalid cross-domain sender'
+    const arbitraryMessage: string = ethersUtils.defaultAbiCoder.encode(
+      ['address'],
+      [ONE_ADDRESS]
+    )
+    await expect(
+      l1_messengerWrapper.verifySender(
+        ONE_ADDRESS,
+        arbitraryMessage
+      )
+    ).to.be.revertedWith(expectedErrorMsg)
   })
 })
