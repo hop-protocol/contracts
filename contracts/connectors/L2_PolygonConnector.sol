f4c838127f4796972a2b06e4f4e6b753116aff31
Shane Fontaine <shane6fontaine@gmail.com>
2021-09-02 04:11:13 -0700

    Error msg fix

--- contracts/connectors/L2_PolygonConnector.sol
+++ contracts/connectors/L2_PolygonConnector.sol
@@ -34,6 +34,6 @@ contract L2_PolygonConnector is FxBaseChildTunnel {
         validateSender(sender)
     {
         (bool success,) = owner.call(data);
-        require(success, "L2_PGLN_CNR: Failed to proxy message");
+        require(success, "L2_PLGN_CNR: Failed to proxy message");
     }
 }
