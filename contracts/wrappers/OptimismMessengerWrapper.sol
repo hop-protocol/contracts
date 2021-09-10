28810da963ee05ffa24fc5915a231d85204a6db3
cwhinfrey <cwhinfrey@gmail.com>
2021-09-09 20:38:52 -0700

    Enable function specific gas limits for Optimism

--- contracts/wrappers/OptimismMessengerWrapper.sol
+++ contracts/wrappers/OptimismMessengerWrapper.sol
@@ -16,20 +16,21 @@ contract OptimismMessengerWrapper is MessengerWrapper, Ownable {
 
     iOVM_L1CrossDomainMessenger public immutable l1MessengerAddress;
     address public immutable l2BridgeAddress;
-    uint256 public l2GasLimit;
+    uint256 public defaultL2GasLimit;
+    mapping (bytes4 => uint256) public l2GasLimitForSignature;
 
     constructor(
         address _l1BridgeAddress,
         address _l2BridgeAddress,
         iOVM_L1CrossDomainMessenger _l1MessengerAddress,
-        uint256 _l2GasLimit
+        uint256 _defaultL2GasLimit
     )
         public
         MessengerWrapper(_l1BridgeAddress)
     {
         l2BridgeAddress = _l2BridgeAddress;
         l1MessengerAddress = _l1MessengerAddress;
-        l2GasLimit = _l2GasLimit;
+        defaultL2GasLimit = _defaultL2GasLimit;
     }
 
     /** 
@@ -37,6 +38,8 @@ contract OptimismMessengerWrapper is MessengerWrapper, Ownable {
      * @param _calldata The data that l2BridgeAddress will be called with
      */
     function sendCrossDomainMessage(bytes memory _calldata) public override onlyL1Bridge {
+        uint256 l2GasLimit = l2GasLimitForCalldata(_calldata);
+
         l1MessengerAddress.sendMessage(
             l2BridgeAddress,
             _calldata,
@@ -50,7 +53,40 @@ contract OptimismMessengerWrapper is MessengerWrapper, Ownable {
         require(l1MessengerAddress.xDomainMessageSender() == l2BridgeAddress, "OVM_MSG_WPR: Invalid cross-domain sender");
     }
 
-    function setL2GasLimit(uint256 _l2GasLimit) external onlyOwner {
-        l2GasLimit = _l2GasLimit;
+    function setDefaultL2GasLimit(uint256 _l2GasLimit) external onlyOwner {
+        defaultL2GasLimit = _l2GasLimit;
+    }
+
+    function setL2GasLimitForSignature(uint256 _l2GasLimit, bytes4 signature) external onlyOwner {
+        l2GasLimitForSignature[signature] = _l2GasLimit;
+    }
+
+    // Private functions
+
+    function l2GasLimitForCalldata(bytes memory _calldata) private view returns (uint256) {
+        uint256 l2GasLimit;
+
+        if (_calldata.length >= 4) {
+            bytes4 functionSignature = bytes4(toUint32(_calldata, 0));
+            l2GasLimit = l2GasLimitForSignature[functionSignature];
+        }
+
+        if (l2GasLimit == 0) {
+            l2GasLimit = defaultL2GasLimit;
+        }
+
+        return l2GasLimit;
+    }
+
+    // source: https://github.com/GNSPS/solidity-bytes-utils/blob/master/contracts/BytesLib.sol
+    function toUint32(bytes memory _bytes, uint256 _start) private pure returns (uint32) {
+        require(_bytes.length >= _start + 4, "OVM_MSG_WPR: out 0f bounds");
+        uint32 tempUint;
+
+        assembly {
+            tempUint := mload(add(add(_bytes, 0x4), _start))
+        }
+
+        return tempUint;
     }
 }
