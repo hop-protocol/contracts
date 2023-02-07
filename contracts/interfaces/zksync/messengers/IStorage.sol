eaf22b827e2baee13b673a4a6c93adc25467abda
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-02-06 18:49:43 -0800

    Add zkSync contracts

---
+++ contracts/interfaces/zksync/messengers/IStorage.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.8.0;
+
+/// @dev The log passed from L2
+/// @param l2ShardId The shard identifier, 0 - rollup, 1 - porter. All other values are not used but are reserved for the future
+/// @param isService A boolean flag that is part of the log along with `key`, `value`, and `sender` address.
+/// This field is required formally but does not have any special meaning.
+/// @param txNumberInBlock The L2 transaction number in a block, in which the log was sent
+/// @param sender The L2 address which sent the log
+/// @param key The 32 bytes of information that was sent in the log
+/// @param value The 32 bytes of information that was sent in the log
+// Both `key` and `value` are arbitrary 32-bytes selected by the log sender
+struct L2Log {
+    uint8 l2ShardId;
+    bool isService;
+    uint16 txNumberInBlock;
+    address sender;
+    bytes32 key;
+    bytes32 value;
+}
+
+/// @dev An arbitrary length message passed from L2
+/// @notice Under the hood it is `L2Log` sent from the special system L2 contract
+/// @param txNumberInBlock The L2 transaction number in a block, in which the message was sent
+/// @param sender The address of the L2 account from which the message was passed
+/// @param data An arbitrary length message
+struct L2Message {
+    uint16 txNumberInBlock;
+    address sender;
+    bytes data;
+}
