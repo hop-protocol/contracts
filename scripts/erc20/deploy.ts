da4d84aefe9e63aacdd8adf58bb951d0d31e5a81
Shane Fontaine <9441295+shanefontaine@users.noreply.github.com>
2023-04-18 16:57:28 -0700

    Add SybilResistant testnet token

--- scripts/erc20/deploy.ts
+++ scripts/erc20/deploy.ts
@@ -1,6 +1,5 @@
 require('dotenv').config()
 import { ethers } from 'hardhat'
-import { BigNumber } from 'ethers'
 
 import { Wallet, utils } from 'zksync-web3'
 import { Deployer } from '@matterlabs/hardhat-zksync-deploy'
@@ -9,51 +8,51 @@ const hre = require('hardhat')
 
 // NOTE: This works with both L1 and L2. Specify the network in the CLI.
 // Example usage:
-// $ npm run deploy:l1-goerli:erc20
-// $ npm run deploy:l2-optimism:erc20
-
-// NOTE: You should not use this for xDai, as their bridge mints a new token.
-// NOTE: Instead, call `relayTokens()` here: 0xA960d095470f7509955d5402e36d9DB984B5C8E2
+// $ npm run deploy:erc20
 async function main () {
   const erc20Name = 'Wrapped Ether'
   const erc20Symbol = 'WETH'
+  const erc20Decimals = 18
 
   const network = await ethers.provider.getNetwork()
   console.log('network:', network)
+  if (network.name === 'unknown') {
+    throw new Error('Unknown network')
+  }
 
   let address: string
   if (hre.network.name === 'zksync_testnet') {
-    address = await deployZkSync(erc20Name, erc20Symbol)
+    address = await deployZkSync(erc20Name, erc20Symbol, erc20Decimals)
   } else {
-    address = await deploy(erc20Name, erc20Symbol)
+    address = await deploy(erc20Name, erc20Symbol, erc20Decimals)
   }
   console.log('erc20 address:', address)
   console.log('deployed bytecode:', await ethers.provider.getCode(address))
   console.log('complete')
 }
 
-async function deploy (name: string, symbol: string): Promise<string> {
+async function deploy (name: string, symbol: string, decimals: number): Promise<string> {
   const signer = (await ethers.getSigners())[0]
   console.log('signer:', await signer.getAddress())
 
   const MockERC20 = await ethers.getContractFactory(
-    'contracts/test/MockERC20WithDeposit.sol:MockERC20WithDeposit',
+    'contracts/test/SybilResistantERC20.sol:SybilResistantERC20',
     { signer }
   )
 
-  const erc20 = await MockERC20.deploy(name, symbol)
+  const erc20 = await MockERC20.deploy(name, symbol, decimals)
   await erc20.deployed()
 
   return erc20.address
 }
 
-async function deployZkSync (name: string, symbol: string): Promise<string> {
+async function deployZkSync (name: string, symbol: string, decimals: number): Promise<string> {
   const wallet = new Wallet(process.env.DEPLOYER_PRIVATE_KEY!)
   const deployer = new Deployer(hre, wallet)
   const artifact = await deployer.loadArtifact(
-    'contracts/test/MockERC20WithDeposit.sol:MockERC20WithDeposit'
+    'contracts/test/SybilResistantERC20.sol:SybilResistantERC20'
   )
-  const l2Contract = await deployer.deploy(artifact, [name, symbol])
+  const l2Contract = await deployer.deploy(artifact, [name, symbol, decimals])
   return l2Contract.address
 }
 
